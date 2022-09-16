d6685c15fc5203ec649cf80e9c2c06f7f18803fd
Prahalad Ramji <prahaladramji@users.noreply.github.com>
2022-09-16 22:32:58 +1000

    add vscode cuelang support

--- applications/vscode.nix
+++ applications/vscode.nix
@@ -3,17 +3,23 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      brettm12345.nixfmt-vscode
-      eamodio.gitlens
-      editorconfig.editorconfig
-      hashicorp.terraform
-      golang.go
-      ms-azuretools.vscode-docker
-      ms-python.python
-      ms-python.vscode-pylance
-    ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        bbenoist.nix
+        brettm12345.nixfmt-vscode
+        eamodio.gitlens
+        editorconfig.editorconfig
+        hashicorp.terraform
+        golang.go
+        ms-azuretools.vscode-docker
+        ms-python.python
+        ms-python.vscode-pylance
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+        name = "vscode-cuelang";
+        publisher = "brody715";
+        version = "0.0.4";
+        sha256 = "g/xYOvo98A5kqDG3hGtfWC+Ap0JULo8I0ruTtNXpt1o=";
+      }];
 
     userSettings = {
       "[nix]"."editor.tabSize" = 2;
