87b9719a8637024dfb6b4149f95459da66ab4389
Prahalad Ramji <prahaladramji@users.noreply.github.com>
2023-07-01 13:22:49 +1000

    switch to using poetry

--- applications/default.nix
+++ applications/default.nix
@@ -32,7 +32,7 @@
       moreutils
       nixfmt
       open-policy-agent
-      pipenv
+      poetry
       pre-commit
       protobuf
       (python310.withPackages (p: with p; [ pre-commit-hooks requests ]))
@@ -114,6 +114,31 @@
         global = { load_dotenv = false; };
         whitelist = { prefix = [ "/home" "/Users" ]; };
       };
+      stdlib = ''
+        layout_poetry() {
+          PYPROJECT_TOML="''${PYPROJECT_TOML: -pyproject.toml}"
+          if [[ ! -f "$PYPROJECT_TOML" ]]; then
+              log_status "No pyproject.toml found. Executing \`poetry init\` to create a \`$PYPROJECT_TOML\` first."
+              poetry init --no-interaction --python ^$(python3 --version 2>/dev/null | cut -d' ' -f2 | cut -d. -f1-2)
+          fi
+
+          if [[ -d ".venv" ]]; then
+              VIRTUAL_ENV="$(pwd)/.venv"
+          else
+              VIRTUAL_ENV=$(poetry env info --path 2>/dev/null ; true)
+          fi
+
+          if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+              log_status "No virtual environment exists. Executing \`poetry install\` to create one."
+              poetry install
+              VIRTUAL_ENV=$(poetry env info --path)
+          fi
+
+          PATH_add "$VIRTUAL_ENV/bin"
+          export POETRY_ACTIVE=1
+          export VIRTUAL_ENV
+        }
+      '';
     };
 
     go = {
